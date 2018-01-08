7b17e9ea5c4ac8bb30bdf10ecba72ea4d18879f7
Alexey Subbotkin <subbotkin147@gmail.com>
2018-01-08 13:23:58 +0200

    Fix

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -2,10 +2,17 @@ import React from 'react';
 import axios from 'axios';
 
 var PERIOD = {
-  TOTAL : {value: 0, name: "Total", query: null},
-  DAY : {value: 1, name: "Day", query: "day"}, 
-  WEEK : {value: 2, name: "Week", query: "week"}, 
-  MONTH : {value: 3, name: "Month", query: "month"}
+  TOTAL : 0,
+  DAY : 1, 
+  WEEK : 2, 
+  MONTH : 3,
+
+  properties: {
+    0: {name: "Total", query: null},
+    1: {name: "Day", query: "day"},
+    2: {name: "Week", query: "week"},
+    3: {name: "Month", query: "month"}
+  }
 };
 
 class Counter extends React.Component {
@@ -13,7 +20,7 @@ class Counter extends React.Component {
     this.setState({
       isLoading: true,
       value: null,
-      period: PERIOD.WEEK
+      period: PERIOD.TOTAL.value
     });
   }
 
@@ -21,14 +28,29 @@ class Counter extends React.Component {
     this.load();
   }
 
+  componentDidUpdate() {
+    if (this.state.isLoading) {
+      this.load();
+    }
+  }
+
   load = () => {
-    let query = 'https://alohabuildtime.herokuapp.com/builds?' + (this.state.period.query ? (this.state.period.query + '=' + this.currentDay()) : '');
+    let queryParameters = {
+      0 : null,
+      1 : "day",
+      2 : "week",
+      3 : "month"
+    }
+
+    let query = 'https://alohabuildtime.herokuapp.com/builds?' + (queryParameters[this.state.period] ? (queryParameters[this.state.period] + '=' + this.currentDay()) : '');
     axios.get(query)
       .then( (response) => {
         this.setState({
           isLoading: false,
           value: response.data.reduce((total, build) => total + build.buildTime, 0)
         });
+
+        console.log(response.data.size);
       })
       .catch( (error) => {
         this.setState({
@@ -45,24 +67,22 @@ class Counter extends React.Component {
     return dateFormat(now, "dd-mm-yyyy");
   }
 
-  updatePeriod = (period) => {
+  updatePeriod = (event) => {
     this.setState({
       isLoading: true,
       value: 'Error',
-      period: period
+      period: event.target.value
     });
-
-    this.load();
   }
 
   render() {
     return <div>
     <div>
       <select id="lang" onChange={this.updatePeriod} value={this.state.period}>
-        <option value={PERIOD.TOTAL}>{PERIOD.TOTAL.name}</option>
-        <option value={PERIOD.DAY}>{PERIOD.DAY.name}</option>
-        <option value={PERIOD.WEEK}>{PERIOD.WEEK.name}</option>
-        <option value={PERIOD.MONTH}>{PERIOD.MONTH.name}</option>
+        <option value={PERIOD.TOTAL}>Total</option>
+        <option value={PERIOD.DAY}>Day</option>
+        <option value={PERIOD.WEEK}>Week</option>
+        <option value={PERIOD.MONTH}>Month</option>
       </select>
     </div>
     <h1> { 
